2ef82fcb40350a71e3b788a4e0098825d02729b2
vinothsb <142011950+vinothsb@users.noreply.github.com>
2023-08-11 22:51:59 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Favorites')
+#streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
