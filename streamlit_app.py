3e807fa6d7daff99a371dc1a12cdd59155bb62fe
vinothsb <142011950+vinothsb@users.noreply.github.com>
2023-08-12 04:35:14 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake-connector-python
+import snowflake-connector
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
